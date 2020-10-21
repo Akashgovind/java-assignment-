91dd5c8083f74be180fa0e28a361e1f1c4552a13
Akashgovind <48796580+Akashgovind@users.noreply.github.com>
2020-10-21 16:24:45 +0530

    Add files via upload

---
+++ day 2 assignment/Main.java
@@ -0,0 +1,16 @@
+public class Main
+{
+  public static void main (String[]args)
+  {
+    employee a = new employee ();
+    employee b = new employee ();
+      a.name = "akash";
+      a.city = "hyd";
+      a.age = 23;
+      b.name = "saurab";
+      b.city = "chennai";
+      b.age = 23;
+      a.display ();
+      b.display ();
+  }
+}
---
+++ day 2 assignment/employe.java
@@ -0,0 +1,13 @@
+public class employee
+{
+  int age;
+  String name = new String ();
+  String city = new String ();
+  public void display ()
+  {
+    System.out.println ("the name is" + name);
+    System.out.println ("the age is" + age);
+    System.out.println ("the city is" + city);
+  }
+
+}
